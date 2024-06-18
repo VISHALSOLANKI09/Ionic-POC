bf2b20fb4856526d8dc0d3ea5afe617d954446ae
Naman Seth <naman.seth@geminisolutions.com>
2024-06-18 15:36:35 +0530

    emulator detection changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Device } from '@awesome-cordova-plugins/device/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  isEmulator: boolean = false;
+  constructor(
+    private platform: Platform,
+    private device: Device,
+  ) {}
+
+  ngOnInit(): void {
+    console.log("oninit....");
+    
+    this.platform.ready().then(() => {
+      this.checkIfEmulator();
+    });
+  }
+
+  checkIfEmulator() {
+    if (this.device.isVirtual) {
+      this.isEmulator = true;
+      console.log('Running on an emulator');
+      (navigator as any).app.exitApp();
+    } else {
+      this.isEmulator = false;
+      console.log('Running on a real device');
+    }
+  }
 }
