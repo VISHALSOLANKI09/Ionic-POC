2a68314f6c7c219f1bc46fb8eba64df3533949be
Naman Seth <naman.seth@geminisolutions.com>
2024-06-27 10:16:28 +0530

    FridaDetect plugin integrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Device } from '@awesome-cordova-plugins/device/ngx';
 import { Platform } from '@ionic/angular';
-
+// @ts-ignore
+import * as fridadetect from  '../../plugins/com.gemini.fridadetect/www/FridaDetectWrapper';
+// @ts-ignore
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  isEmulator: boolean = false;
+  // isEmulator: boolean = true;
   constructor(
     private platform: Platform,
     private device: Device,
@@ -16,20 +19,44 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     console.log("oninit....");
-    
     this.platform.ready().then(() => {
-      this.checkIfEmulator();
+      this.checkFridaDetection();
+    }); 
+  }
+
+  checkFridaDetection() {
+    fridadetect.fridadetect("0", (result: string) => {
+        console.log('result-' + result);
+        this.showSuccessAlert()
+      }, (error: string) => {
+        console.log('result-error' + error);
+        this.showErrorAlert(error);
+    })
+  }
+
+  showErrorAlert(text: string) {
+    Swal.fire({
+      title: 'Error!',
+      text: text,
+      icon: 'error',
+      confirmButtonText: 'OK'
+    }).then((result: any) => {
+      if (result.isConfirmed) {
+        fridadetect.closeapp("0", (result: string) => {
+          console.log('result-' + result);
+          }, (error: string) => {
+          console.log('result-error' + error);
+        })
+      }
     });
   }
 
-  checkIfEmulator() {
-    if (this.device.isVirtual) {
-      this.isEmulator = true;
-      console.log('Running on an emulator');
-      (navigator as any).app.exitApp();
-    } else {
-      this.isEmulator = false;
-      console.log('Running on a real device');
-    }
+  showSuccessAlert() {
+    Swal.fire({
+      title: 'Success!',
+      text: 'Device is secure!!',
+      icon: 'success',
+      confirmButtonText: 'OK'
+    })
   }
 }
